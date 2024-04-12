e59bc99e4dec99c5c4176fd060d0edf60681d59c
HooDini72 <120455106+HooDini72@users.noreply.github.com>
2024-04-12 23:29:55 +0200

    Login Front End

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var usersRouter = require('./routes/login');
 
 var app = express();
 
@@ -14,7 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get(["/login", "/register"], function(req, res, next) {
+    res.sendFile(path.join(__dirname, 'public', '/login.html'));
+});
+  
 
 module.exports = app;
---
+++ public/javascripts/appLogin.js
@@ -0,0 +1,66 @@
+let app = Vue.createApp({
+    data() {
+        return {
+            loginEmail: '',
+            loginPassword: '',
+            registerEmail: '',
+            registerPassword: '',
+            registerPasswordRepeat: ''
+        };
+    },
+    methods: {
+        loginFetch() {
+
+            const url = new URL('http://localhost:3000/user_handling/login'); //Route for login
+            url.search = new URLSearchParams({
+                mail: this.loginEmail,
+                pw: this.loginPassword
+            }).toString();
+
+            fetch(url)  //Send Request
+                .then(resp => {
+                    if (!response.ok) {
+                        throw new Error('Login failed');
+                    }
+                    return resp.json();
+                })
+                .then(data => {
+                    localStorage.setItem('authToken', data.token); //save key/value pairs in the browser --> key: authToke, value: data.token
+                    alert('Registration successful');
+                    window.location.href = '/index.html';
+                })
+                .catch(error => { 
+                    console.error('Error:', error);
+                    alert('Login failed');
+                });
+
+        },
+        registerFetch() {
+            if(this.registerPassword != this.registerPasswordRepeat){
+                alert('Password and repeat Password differ');
+            }
+
+            const url = new URL('http://localhost:3000/user_handling/register'); //Route for registration
+            url.search = new URLSearchParams({
+                mail: this.registerEmail,
+                pw: this.registerPassword
+            }).toString();
+
+            fetch(url)
+                .then(resp => {
+                    if (!resp.ok) {
+                        throw new Error('Registration failed');
+                    }
+                    return resp.json();
+                })
+                .then(data => {
+                    alert('Registration successful');
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    alert('Registration failed');
+                });
+        }
+    }
+
+}).mount("#app");
